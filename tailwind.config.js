d8f5fb0c8d0417a62683d62cd85de83b15528dda
Antek <tonylearnscoding@gmail.com>
2023-07-23 20:40:53 +0200

    [AK-2] Extend tailwind theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,11 +8,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        fira: 'var(--font-fira)',
+        poppins: 'var(--font-poppins)',
+      },
+      colors: {
+        green_bg: '#24393F',
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwind-utopia')],
 };
